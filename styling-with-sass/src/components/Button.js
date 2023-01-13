71baeafe95794ce4470323c0f179ac88b921ff28
shypang <smartdragon417@gmail.com>
2023-01-13 23:26:49 +0900

    button 컴포넌트 사이즈, 칼라, rest , outline, rest 스터디함

--- styling-with-sass/src/components/Button.js
+++ styling-with-sass/src/components/Button.js
@@ -1,15 +1,21 @@
 import React from "react";
-import "./Button.scss";
 import classNames from "classnames";
+import "./Button.scss";
 
-function Button({ children, size }) {
+function Button({ children, size, color, outline, fullWidth, ...rest }) {
   return (
-    <button className={classNames("Button", size).join(" ")}>{children}</button>
+    <button
+      className={classNames("Button", size, color, { outline, fullWidth })}
+      {...rest}
+    >
+      {children}
+    </button>
   );
 }
 
 Button.defaultProps = {
   size: "medium",
+  color: "blue",
 };
 
 export default Button;
