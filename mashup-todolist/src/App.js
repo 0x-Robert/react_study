8979e498eec757a7b6f32f7ffd0b110e5a59fac5
shypang <smartdragon417@gmail.com>
2023-01-16 23:09:33 +0900

    ContextAPI를 활용한 상태관리

--- mashup-todolist/src/App.js
+++ mashup-todolist/src/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import { createGlobalStyle } from "styled-components";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoHead from "./components/TodoHead";
-// 글로벌 스타일을 추가하고 싶을 땐 createGlobalStyle 이라는 것을 사용합니다. 이 함수를 사용하면 컴포넌트가 만들어지는데, 이 컴포넌트를 렌더링하면 됩니다.
+import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
+
 const GlobalStyle = createGlobalStyle`
-body {
-  background: #e9ecef;
-}`;
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <TodoTemplate>
-        <TodoHead></TodoHead>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
       </TodoTemplate>
     </>
   );
