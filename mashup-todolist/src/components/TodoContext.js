7fd78a3b251399a68785e8809df79b3167c5bbc5
shypang <smartdragon417@gmail.com>
2023-01-17 23:29:38 +0900

    기능구현

--- mashup-todolist/src/components/TodoContext.js
+++ mashup-todolist/src/components/TodoContext.js
@@ -1,13 +1,98 @@
+// import React, { useReducer, createContext, useContext, useRef } from 'react';
+
+// const initialTodos = [
+//   {
+//     id: 1,
+//     text: '프로젝트 생성하기',
+//     done: true
+//   },
+//   {
+//     id: 2,
+//     text: '컴포넌트 스타일링하기',
+//     done: true
+//   },
+//   {
+//     id: 3,
+//     text: 'Context 만들기',
+//     done: false
+//   },
+//   {
+//     id: 4,
+//     text: '기능 구현하기',
+//     done: false
+//   }
+// ];
+
+// function todoReducer(state, action) {
+//   switch (action.type) {
+//     case 'CREATE':
+//       return state.concat(action.todo);
+//     case 'TOGGLE':
+//       return state.map(todo =>
+//         todo.id === action.id ? { ...todo, done: !todo.done } : todo
+//       );
+//     case 'REMOVE':
+//       return state.filter(todo => todo.id !== action.id);
+//     default:
+//       throw new Error(`Unhandled action type: ${action.type}`);
+//   }
+// }
+
+// const TodoStateContext = createContext();
+// const TodoDispatchContext = createContext();
+// const TodoNextIdContext = createContext();
+
+// export function TodoProvider({ children }) {
+//   const [state, dispatch] = useReducer(todoReducer, initialTodos);
+//   const nextId = useRef(5);
+
+//   return (
+//     <TodoStateContext.Provider value={state}>
+//       <TodoDispatchContext.Provider value={dispatch}>
+//         <TodoNextIdContext.Provider value={nextId}>
+//           {children}
+//         </TodoNextIdContext.Provider>
+//       </TodoDispatchContext.Provider>
+//     </TodoStateContext.Provider>
+//   );
+// }
+
+// export function useTodoState() {
+//   const context = useContext(TodoStateContext);
+//   if (!context) {
+//     throw new Error('Cannot find TodoProvider');
+//   }
+//   return context;
+// }
+
+// export function useTodoDispatch() {
+//   const context = useContext(TodoDispatchContext);
+//   if (!context) {
+//     throw new Error('Cannot find TodoProvider');
+//   }
+//   return context;
+// }
+
+// export function useTodoNextId() {
+//   const context = useContext(TodoNextIdContext);
+//   if (!context) {
+//     throw new Error('Cannot find TodoProvider');
+//   }
+//   return context;
+// }
+
 import React, { useReducer, createContext, useContext, useRef } from "react";
 
 const initialTodos = [
   {
     id: 1,
     text: "프로젝트 생성하기",
+    done: true,
   },
   {
     id: 2,
     text: "컴포넌트 스타일링하기",
+    done: true,
   },
   {
     id: 3,
@@ -78,15 +163,3 @@ export function useTodoNextId() {
   }
   return context;
 }
-
-// export function useTodoState() {
-//   return useContext(TodoStateContext);
-// }
-// export function useTodoDispatch() {
-//   return useContext(TodoDispatchContext);
-// }
-
-// export function useTodoNextId() {
-//   return useContext(TodoNextIdContext);
-// }
-// nextId 값을 위한 Context 를 만들 때에도 마찬가지로 useTodoNextId 라는 커스텀 Hook을 따로 만들어주었습니다.
--- mashup-todolist/src/components/TodoHead.js
+++ mashup-todolist/src/components/TodoHead.js
@@ -1,25 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import { useTodoState } from "./TodoContext";
+
 const TodoHeadBlock = styled.div`
   padding-top: 48px;
   padding-left: 32px;
   padding-right: 32px;
   padding-bottom: 24px;
   border-bottom: 1px solid #e9ecef;
-
   h1 {
     margin: 0;
     font-size: 36px;
     color: #343a40;
   }
-
   .day {
     margin-top: 4px;
     color: #868e96;
     font-size: 21px;
   }
-
   .tasks-left {
     color: #20c997;
     font-size: 18px;
@@ -30,12 +28,13 @@ const TodoHeadBlock = styled.div`
 
 function TodoHead() {
   const todos = useTodoState();
-  console.log(todos);
+  const undoneTasks = todos.filter((todo) => !todo.done);
+
   return (
     <TodoHeadBlock>
-      <h1>2023/ 01/ 14</h1>
-      <div className="day">일요일</div>
-      <div className="tasks-left">할 일 2개 남음</div>
+      <h1>2019년 7월 10일</h1>
+      <div className="day">수요일</div>
+      <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
     </TodoHeadBlock>
   );
 }
