f1bd405d77657c90f9d424700cadd804fbaa1944
shypang <smartdragon417@gmail.com>
2023-01-18 23:28:35 +0900

    기능구현 끝

--- mashup-todolist/src/components/TodoList.js
+++ mashup-todolist/src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import TodoItem from "./TodoItem";
+import { useTodoState } from "./TodoContext";
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -8,14 +9,19 @@ const TodoListBlock = styled.div`
   padding-bottom: 48px;
   overflow-y: auto;
 `;
-
 function TodoList() {
+  const todos = useTodoState();
+
   return (
     <TodoListBlock>
-      <TodoItem text="프로젝트 생성하기" done={true} />
-      <TodoItem text="컴포넌트 스타일링 하기" done={true} />
-      <TodoItem text="Context 만들기" done={false} />
-      <TodoItem text="기능 구현하기" done={false} />
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}
+        />
+      ))}
     </TodoListBlock>
   );
 }
