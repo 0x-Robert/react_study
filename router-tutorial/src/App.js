bedc279f6effddddf47569fa5cec3c1f7f41dc08
shypang <smartdragon417@gmail.com>
2023-01-21 22:47:59 +0900

    리액트 라우터

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Route, Routes, Link  } from 'react-router-dom';
+import About from "./About"
+import Home from "./Home";
+
+
+const App = ()=> {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr/>
+      <Routes>
+      <Route path="/"  element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
+
+
+// // version 5
+// import { Route } from 'react-router-dom';
+
+// <div>
+//       <Route path="/" exact={true} component={Home} />
+// </div>
+// // version 6
+// import { Route, Routes } from "react-router-dom";
+
+// <div>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//       </Routes>
+// </div>
