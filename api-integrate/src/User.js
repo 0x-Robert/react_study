5243f6c24cce80e2b24163605dde944c7a8db543
shypang <smartdragon417@gmail.com>
2023-01-21 18:39:13 +0900

    context api를 이용한 상태관리, 리팩토링 전

--- api-integrate/src/User.js
+++ api-integrate/src/User.js
@@ -1,3 +1,4 @@
+// 라이브러리 이용한 함수
 // import React from 'react';
 // import axios from 'axios';
 // import useAsync from './useAsync';
@@ -37,36 +38,65 @@
 
 
 
-import React from 'react';
-import axios from 'axios';
-import { useAsync } from 'react-async';
+// import React from 'react';
+// import axios from 'axios';
+// import { useAsync } from 'react-async';
 
-async function getUser({ id }) {
-  const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/users/${id}'
-  );
-  return response.data;
+// async function getUser({ id }) {
+//   const response = await axios.get(
+//     'https://jsonplaceholder.typicode.com/users/${id}'
+//   );
+//   return response.data;
+// }
+
+// function User({ id }) {
+//   const { data: user, error, isLoading } = useAsync({
+//     promiseFn: getUser,
+//     id,
+//     watch: id
+//   });
+
+//   if (isLoading) return <div>로딩중..</div>;
+//   if (error) return <div>에러가 발생했습니다</div>;
+//   if (!user) return null;
+
+//   return (
+//     <div>
+//       <h2>{user.username}</h2>
+//       <p>
+//         <b>Email:</b> {user.email}
+//       </p>
+//     </div>
+//   );
+// }
+
+// export default User;
+
+
+import React, {useEffect } from 'react';
+import {useUsersState, useUsersDispatch, getUser} from "./UsersContext";
+
+function User({id}){
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
+    useEffect(()=>{
+        getUser(dispatch, id);
+    }, [dispatch, id]);
+
+    const {data: user, loading, error} = state.user; 
+
+    if(loading) return <div>로딩중...</div>
+    if(error) return <div>에러가 발생했습니다.</div>
+    if(!user) return null; 
+    return (
+        <div>
+            <h2>{user.username}</h2>
+            <p>
+                <b>Email:</b> {user.email}
+            </p>
+        </div>
+    )
 }
 
-function User({ id }) {
-  const { data: user, error, isLoading } = useAsync({
-    promiseFn: getUser,
-    id,
-    watch: id
-  });
+export default User; 
 
-  if (isLoading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-  if (!user) return null;
-
-  return (
-    <div>
-      <h2>{user.username}</h2>
-      <p>
-        <b>Email:</b> {user.email}
-      </p>
-    </div>
-  );
-}
-
-export default User;
--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,3 +1,5 @@
+//프로젝트시 직접 만든 라이브러리를 사용하는것이 좋다고 함 
+// //다음은 직접 만든 라이브러리임 참고
 // import React from 'react';
 // import axios from 'axios';
 // import useAsync from './useAsync';
@@ -36,15 +38,11 @@
 // export default Users;
 
 
-
-//라이브러리 이용함
 // import React, { useState } from 'react';
 // import axios from 'axios';
 // import { useAsync } from 'react-async';
 // import User from './User';
 
-// // useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
-// // 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
 // async function getUsers() {
 //   const response = await axios.get(
 //     'https://jsonplaceholder.typicode.com/users'
@@ -54,13 +52,13 @@
 
 // function Users() {
 //   const [userId, setUserId] = useState(null);
-//   const { data: users, error, isLoading, run } = useAsync({
-//     deferFn: getUsers
+//   const { data: users, error, isLoading, reload } = useAsync({
+//     promiseFn: getUsers
 //   });
 
 //   if (isLoading) return <div>로딩중..</div>;
 //   if (error) return <div>에러가 발생했습니다</div>;
-//   if (!users) return <button onClick={run}>불러오기</button>;
+//   if (!users) return <button onClick={reload}>불러오기</button>;
 
 //   return (
 //     <>
@@ -75,99 +73,59 @@
 //           </li>
 //         ))}
 //       </ul>
-//       <button onClick={run}>다시 불러오기</button>
+//       <button onClick={reload}>다시 불러오기</button>
 //       {userId && <User id={userId} />}
 //     </>
 //   );
 // }
 
-// export default Users;
-
-
-
-
-// import React from 'react';
-// import axios from 'axios';
-// import useAsync from './useAsync';
-
-
-// // useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
-// // 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
-// async function getUsers(){
-//   const response = await axios.get(
-//     'https://jsonplaceholder.typicode.com/users'
-//   );
-//   return response.data; 
-// }
-
-// function Users(){
-//   const[state, refetch] = useAsync(getUsers, []);
-
-//   const{loading, data:users, error} = state; 
-
-//   if(loading) return <div>로딩중..</div>
-//   if(error) return <div>에러가 발생했습니다. </div>
-//   if(!users) return null; 
-
-//   return (
-//     <>
-//     <ul>
-//       {users.map(user=>(
-//         <li key={user.id}>
-//           {user.username} ({user.name})
-//         </li>
-//       ))}
-//     </ul>
-//     <button onClick={refetch}>다시불러오기</button>
-//     </>
-//   )
-// }
+// //라이브러리를 쓰려면 다음 이슈를 참고해야됨
+// //react 18 upgrade 버전에서 해당 라이브러리가 제대로 동작하지 않는 것 같습니다.
+// // 해당 라이브러리의 github의 issue를 확인해본 결과 react 프로젝트에서<React.StrictMode /> 를 제거하면 동작하는 것 같습니다.
 
 // export default Users;
 
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useAsync } from 'react-async';
-import User from './User';
 
-async function getUsers() {
-  const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/users'
-  );
-  return response.data;
-}
+
+
+
+import React, {useState} from 'react';
+import {useUsersState, useUsersDispatch, getUsers} from "./UsersContext"
+import User from "./User";
 
 function Users() {
-  const [userId, setUserId] = useState(null);
-  const { data: users, error, isLoading, reload } = useAsync({
-    promiseFn: getUsers
-  });
 
-  if (isLoading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return <button onClick={reload}>불러오기</button>;
+  const [userId, setUserId] = useState(null);
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
+
+  const {data: users, loading, error} = state.users; 
+  const fetchData = () => {
+    getUsers(dispatch);
+  };
+
+  if(loading) return <div>로딩중</div>
+  if(error) return <div>에러가 발생했습니다.</div>
+  if(!users) return <button onClick={fetchData}>불러오기</button>
 
   return (
     <>
-      <ul>
-        {users.map(user => (
-          <li
-            key={user.id}
-            onClick={() => setUserId(user.id)}
-            style={{ cursor: 'pointer' }}
-          >
-            {user.username} ({user.name})
-          </li>
-        ))}
-      </ul>
-      <button onClick={reload}>다시 불러오기</button>
-      {userId && <User id={userId} />}
+    <ul>
+    {users.map(user=>(
+
+      <li key={user.id}
+      onClick={()=> setUserId(user.id)}
+      style={{cursor: 'pointer'}}>
+        {user.username} ({user.name})
+      </li>
+    ))}
+    </ul>
+    <button onClick={fetchData}>다시 불러오기</button>
+    {userId && <User id={userId}/>}
     </>
-  );
+  )
 }
 
-//라이브러리를 쓰려면 다음 이슈를 참고해야됨
-//react 18 upgrade 버전에서 해당 라이브러리가 제대로 동작하지 않는 것 같습니다.
-// 해당 라이브러리의 github의 issue를 확인해본 결과 react 프로젝트에서<React.StrictMode /> 를 제거하면 동작하는 것 같습니다.
 
-export default Users;
+export default Users; 
+
