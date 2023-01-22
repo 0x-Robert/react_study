29221c551a279d2448bb130d55ca8ef639150ea5
shypang <smartdragon417@gmail.com>
2023-01-23 08:06:52 +0900

    리덕스 할 일 목록 구현

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 function App() {
   return (
     <div>
 <CounterContainer/>
+<hr/>
+<TodosContainer/>
     </div>
   );
 }
