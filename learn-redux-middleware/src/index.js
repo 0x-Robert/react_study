bfcca9c303cf3681b8dfda225843c1c9636fce66
shypang <smartdragon417@gmail.com>
2023-01-23 22:31:23 +0900

    redux 7.3

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -1,3 +1,6 @@
+// yarn add redux-logger
+// yarn add redux-devtools-extension
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -5,14 +8,27 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './modules';
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import { createStore, applyMiddleware } from 'redux';
+//import myLogger from './middlewaress/myLogger';
+import logger from "redux-logger";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 //프로젝트에 리덕스를 적용할 때 index.js에서 루트리듀서를 불러와서 새로운 스토어를 만들고
 //provider를 사용해서 프로젝트에 적용함
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const store = createStore(rootReducer);
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
+
+
+//const store = createStore(rootReducer,applyMiddleware( logger));
+//여러개의 미들웨어를 적용할 수 있습니다. 
+
 
 root.render(
   <React.StrictMode>
