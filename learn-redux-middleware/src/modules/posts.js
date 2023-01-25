c6042b820b459b6162578e69fbd5841e55a6588a
shypang <smartdragon417@gmail.com>
2023-01-25 23:20:23 +0900

    redux 7.12 posts.js 수정

--- learn-redux-middleware/src/modules/posts.js
+++ learn-redux-middleware/src/modules/posts.js
@@ -441,29 +441,30 @@ const GET_POSTS_ERROR = 'GET_POSTS_ERROR'; // 요청 실패
 const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
-const GO_TO_HOME = 'GO_TO_HOME';
+// const GO_TO_HOME = 'GO_TO_HOME';
 
 export const getPosts = () => ({ type: GET_POSTS });
 export const getPost = id => ({ type: GET_POST, payload: id, meta: id });
-export const goToHome = () => ({ type: GO_TO_HOME })
+// export const goToHome = () => ({ type: GO_TO_HOME })
 
 
 const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
 const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
-function* goToHomeSaga() {
-  const history = yield getContext('history');
-  history.push('/');
-}
-
-// export const goToHome = (navigate) => (dispatch, getState) => {
+// function* goToHomeSaga(navigate) {
+//   const history = yield getContext('history');
 //   navigate("/");
 // }
+
+export const goToHome = (navigate) => (dispatch, getState) => {
+  navigate("/");
+}
 
 // 사가들을 합치기
 export function* postsSaga() {
   yield takeEvery(GET_POSTS, getPostsSaga);
   yield takeEvery(GET_POST, getPostSaga);
-  yield takeEvery(GO_TO_HOME, goToHomeSaga);
+//     yield takeEvery(GO_TO_HOME, goToHomeSaga);
+// }
 }
 
 // initialState 쪽도 반복되는 코드를 initial() 함수를 사용해서 리팩토링 했습니다.
