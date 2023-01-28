0b9f2f157e9741e9afa547ec5c96c5c8c2c10f76
shypang <smartdragon417@gmail.com>
2023-01-28 18:33:57 +0900

    8.4 typescript and context api

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // import Counter from "./Counter"
 import MyForm from './MyForm';
 import ReducerSample from './RdeucerSample';
-
+import { SampleProvider } from './SampleContext';
 
 const App: React.FC = () => {
  // return <ReducerSample />;
@@ -13,7 +13,11 @@ const onSubmit = (form: {name: string; description: string }) => {
   console.log(form);
 }
 // return <MyForm onSubmit={onSubmit}/>;
-return <ReducerSample/>
+return (
+  <SampleProvider>
+    <ReducerSample/>
+  </SampleProvider>
+)
 };
 
 export default App;
